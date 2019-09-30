336e6d8873c71fd8d2234da2765127fa647d3ce7
imlegend19 <mahengandhi19@gmail.com>
2019-09-30 18:58:55 +0530

    added worksheet 4 solutions

--- Work Sheet 4/script.cpp
+++ Work Sheet 4/script.cpp
@@ -2,7 +2,12 @@
 #include <cmath>
 
 typedef long double ld;
-typedef ld (*Function) (ld x);
+
+typedef ld (*Function)(ld x);
+
+const ld epsilon = 0.0001;
+const ld gm = (1 + sqrt(5)) / 2;
+const ld dh = sqrt(DBL_EPSILON);
 
 using namespace std;
 
@@ -26,6 +31,61 @@ ld q1i(ld x) { return 0.5 * powl(x, 2) + (2 * cos(x)); }
 
 ld q1j(ld x) { return powl(x, 100); }
 
+ld derivative(Function f, ld x) { return (f(x + dh) - f(x - dh)) / 2 * dh; }
+
+ld double_derivative(Function f, ld x) { return (f(x + dh) - 2 * f(x) + f(x - dh)) / powl(dh, 2); }
+
+//for extrema
+ld *newton_raphson(Function f, ld a) {
+    ld h = derivative(f, a) / double_derivative(f, a);
+
+    int eval = 0;
+    while (a <= 5) {
+        h = derivative(f, a) / double_derivative(f, a);
+        a -= h;
+        eval += 1;
+    }
+
+    return new ld[3]{a, f(a), (ld) eval};
+}
+
+// for extrema
+ld *golden_search(Function f, ld a, ld b) {
+    ld L0 = b - a;
+    ld prevRatio = 1 / gm * L0;
+    ld x1, x2;
+
+    int eval = 2;
+    while (prevRatio > epsilon) {
+        prevRatio = powl(1 / gm, eval) * L0;
+        x1 = a + prevRatio;
+        x2 = b - prevRatio;
+
+        if (x1 < x2) {
+            if (f(x1) > f(x2)) {
+                b = x2;
+            } else {
+                a = x1;
+            }
+        } else {
+            if (f(x1) > f(x2)) {
+                a = x2;
+            } else {
+                b = x1;
+            }
+        }
+
+        eval += 1;
+    }
+
+    if (f(a) > f(b)) {
+        return new ld[3]{a, f(a), (ld) eval - 1};
+    } else {
+        return new ld[3]{b, f(b), (ld) eval - 1};
+    }
+}
+
+// for extrema
 ld *quadratic_fit_search(ld a, ld b, ld c, Function f, int n) {
     ld ya, yb, yc;
     ya = f(a);
@@ -40,33 +100,33 @@ ld *quadratic_fit_search(ld a, ld b, ld c, Function f, int n) {
 
         if (x > b) {
             if (yx > yb) {
-                c = x;
-                yc = yx;
-            } else {
                 a = b;
                 ya = yb;
                 b = x;
                 yb = yx;
+            } else {
+                c = x;
+                yc = yx;
             }
         } else if (x < b) {
             if (yx > yb) {
-                a = x;
-                ya = yx;
-            } else {
                 c = b;
                 yc = yb;
                 b = x;
                 yb = yx;
+            } else {
+                a = x;
+                ya = yx;
             }
         }
     }
 
-    if (ya <= yb && ya <= yc)
-        return new ld[2] {a, ya};
-    else if (b <= a && b <= c)
-        return new ld[2] {b, yb};
+    if (ya >= yb && ya >= yc)
+        return new ld[2]{a, ya};
+    else if (yb >= ya && yb >= yc)
+        return new ld[2]{b, yb};
     else
-        return new ld[2] {c, yc};
+        return new ld[2]{c, yc};
 }
 
 int main() {
@@ -78,9 +138,40 @@ int main() {
     cout << "Quadratic Fit Search" << endl;
     cout << "--------------------" << endl;
 
-    for (int i=0; i<10; i++) {
-        ld *min = quadratic_fit_search(1, 3, 5, funcs[i], 10);
-        cout << "The minimum value for Q1-(" << letters[i] << ") is " << min[1] << " at " << min[0] << endl;
+    for (int i = 0; i < 10; i++) {
+        ld *max;
+        if (i != 3) {
+            max = quadratic_fit_search(0, 1 / exp(1), 5, funcs[i], 10);
+        } else {
+            max = quadratic_fit_search(0.5, 0.5 / exp(1), 5, funcs[i], 10);
+        }
+        cout << "The maximum value for Q1-(" << letters[i] << ") is " << max[1] << " at " << max[0] << endl;
+    }
+
+    cout << "--------------------" << endl;
+    cout << "--------------------" << endl;
+
+    cout << "1D Line Search - Golden Section Search" << endl;
+    cout << "--------------------" << endl;
+
+    for (int i = 0; i < 10; i++) {
+        ld *max = golden_search(funcs[i], 0, 5);
+        cout << "The maximum value for Q1-(" << letters[i] << ") is " << max[1] << " at " << max[0] << " in "
+             << (int) max[2]
+             << " evaluations. " << endl;
+    }
+
+    cout << "--------------------" << endl;
+    cout << "--------------------" << endl;
+
+    cout << "Newton-Raphson Method" << endl;
+    cout << "--------------------" << endl;
+
+    for (int i = 0; i < 10; i++) {
+        ld *max = newton_raphson(funcs[i], 1);
+        cout << "The maximum value for Q1-(" << letters[i] << ") is " << max[1] << " at " << max[0] << " in "
+             << (int) max[2]
+             << " evaluations. " << endl;
     }
 
     cout << "--------------------" << endl;
--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -48,5 +48,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/WS3.dir/DependInfo.cmake"
+  "CMakeFiles/WS4.dir/DependInfo.cmake"
   )
